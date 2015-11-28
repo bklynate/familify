65a6a32acb608938164ffee4b1b56f2a79ca2a42
bklynate <nathanielcfa@gmail.com>
2015-11-28 13:24:18 -0500

    changed route file, added static_pages tests, ALL TESTS PASSED

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,16 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Familify | Follow Family Events & Discussions"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Familify Help | Familify FAQ"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "Familify About | We are a 'family-' centric communication platform"
   end
 
 end
