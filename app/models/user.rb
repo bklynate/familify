90045020542ad1b5aff2aa1169688805f4a1dfbc
bklynate <nathanielcfa@gmail.com>
2015-11-29 01:02:22 -0500

    working on the user model, added secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 class User < ActiveRecord::Base
-  has_secure_password
-  
   validates :name, presence: true,
                    length: { maximum: 15 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true,
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true
+  has_secure_password
 end
