5ce1f9b814ac92e8d2f4f5db9450fef7242040dd
bklynate <nathanielcfa@gmail.com>
2015-12-02 18:46:19 -0500

    added users resource, fixed views path 'user/show.html.erb' to 'users/show.html.erb', added show.html.erb - btw

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -9,7 +9,7 @@ class UserTest < ActiveSupport::TestCase
     @user = User.new(name: "Example User", email: "user@example.com",
     password: "foobar", password_confirmation: "foobar")
   end
-    
+
   test "email addresses should be unique" do
     duplicate_user = @user.dup
     duplicate_user.email = @user.email.upcase
@@ -22,5 +22,10 @@ class UserTest < ActiveSupport::TestCase
     @user.email = mixed_case_email
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
 end
