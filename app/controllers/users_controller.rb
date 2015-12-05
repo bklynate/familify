0482b459659616b01ea856a8a6a4c7939502e935
bklynate <nathanielcfa@gmail.com>
2015-12-05 00:35:01 -0500

    working signup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,31 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def index
+    @user = User.all
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = "Account successfully created!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    @user = params.require(:user).permit(:name,:email,:password,
+                                          :password_confirmation)
   end
 end
